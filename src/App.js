635fd346ef33b9ff3e6c115563a778a7739f37ba
georgewakhu270 <georgewakhu270@gmail.com>
2025-11-12 09:46:32 +0000

    build the first

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import "./styles.css";
 
 export default function App() {
   const [items, setItems] = useState([]);
+  const [names, setNames] = useState("");
+  const [quantity, setQuantity] = useState(0);
+  const [price, setPrice] = useState(0.0);
   return (
     <div className="App">
       <div>
         <h1>Shopping List</h1>
         <div>
           <form>
-            <label>Item</label>
+            <label>Item Name</label>
             <input type="text" />
             <label>Price</label>
             <input type="number" />
             <label>Quantity</label>
             <input type="number" />
+            <p>The price is </p>
             <button>submit</button>
           </form>
         </div>
