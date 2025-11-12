bcc69252dd2615dd4dea00264a74e56f4691cb31
georgewakhu270 <georgewakhu270@gmail.com>
2025-11-12 10:44:44 +0000

    added form

--- src/App.js
+++ src/App.js
@@ -7,20 +7,33 @@ export default function App() {
   const [names, setNames] = useState("");
   const [quantity, setQuantity] = useState(0);
   const [price, setPrice] = useState(0.0);
+  const [testPrice, setTestPrice] = useState(0.0);
   return (
     <div className="App">
       <div>
         <h1>Shopping List</h1>
         <div className="form-div">
           <div className="div-border">
+            <h2>Add New Item</h2>
             <form>
-              <label>Item Name</label>
-              <input type="text" />
-              <label>Price</label>
-              <input type="number" />
-              <label>Quantity</label>
-              <input type="number" />
-              <p>The price is </p>
+              <div className="form-group">
+                <label>Item Name</label>
+                <input type="text" />
+              </div>
+              <div className="form-group">
+                <label>Price</label>
+                <input type="number" />
+              </div>
+              <div className="form-group">
+                <label>Quantity</label>
+                <input type="number" />
+              </div>
+              <div className="price-display">
+                <p>
+                  The price is
+                  <span className="price-value">$ {testPrice}</span>
+                </p>
+              </div>
               <button>submit</button>
             </form>
           </div>
