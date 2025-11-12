99d61b6f6683e4a044b3c50a19f25c6195d58fbe
georgewakhu270 <georgewakhu270@gmail.com>
2025-11-12 09:22:40 +0000

    added form

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ export default function App() {
     <div className="App">
       <div>
         <h1>Shopping List</h1>
+        <div>
+          <form>
+            <label>Item</label>
+            <input type="text" />
+            <label>Price</label>
+            <input type="number" />
+            <label>Quantity</label>
+            <input type="number" />
+            <button>submit</button>
+          </form>
+        </div>
       </div>
     </div>
   );
