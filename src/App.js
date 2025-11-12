f5fbf798f118d2a0160d3db507f554481869e4b8
georgewakhu270 <georgewakhu270@gmail.com>
2025-11-12 11:28:03 +0000

    done with the form js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import "./App.css";
 export default function App() {
   const [items, setItems] = useState([]);
   const [names, setNames] = useState("");
-  const [quantity, setQuantity] = useState(0);
+  const [quantity, setQuantity] = useState(1);
   const [price, setPrice] = useState(0.0);
-  const [testPrice, setTestPrice] = useState(0.0);
+
   return (
     <div className="App">
       <div>
@@ -18,20 +18,33 @@ export default function App() {
             <form>
               <div className="form-group">
                 <label>Item Name</label>
-                <input type="text" />
+                <input
+                  type="text"
+                  value={names}
+                  placeholder="e.g Sugar"
+                  onChange={(e) => setNames(e.target.value)}
+                />
               </div>
               <div className="form-group">
                 <label>Price ($)</label>
-                <input type="number" />
+                <input
+                  type="number"
+                  value={price}
+                  onChange={(e) => setPrice(e.target.value)}
+                />
               </div>
               <div className="form-group">
                 <label>Quantity</label>
-                <input type="number" />
+                <input
+                  type="number"
+                  value={quantity}
+                  onChange={(e) => setQuantity(e.target.value)}
+                />
               </div>
               <div className="price-display">
                 <p>
                   The price is
-                  <span className="price-value">$ {testPrice}</span>
+                  <span className="price-value">$ {price * quantity}</span>
                 </p>
               </div>
               <button>submit</button>
