9dffe12fba939320f5497f3aa0866181b9d93aa5
georgewakhu270 <georgewakhu270@gmail.com>
2025-11-13 08:38:55 +0000

    added a popup

--- src/App.js
+++ src/App.js
@@ -7,49 +7,72 @@ export default function App() {
   const [names, setNames] = useState("");
   const [quantity, setQuantity] = useState(1);
   const [price, setPrice] = useState(0.0);
+  const [totalPrice, setTotalPrice] = useState(0.0);
+  const [popup, setPopup] = useState(false);
+
+  const addItem = (e) => {
+    e.preventDefault();
+    setItems([...items, { name: names, quantity: quantity, price: price }]);
+    setTotalPrice(totalPrice + price);
+    setPopup(false);
+  };
 
   return (
     <div className="App">
       <div>
         <h1>Shopping List</h1>
-        <div className="form-div">
-          <div className="div-border">
-            <h2>Add New Item</h2>
-            <form>
-              <div className="form-group">
-                <label>Item Name</label>
-                <input
-                  type="text"
-                  value={names}
-                  placeholder="e.g Sugar"
-                  onChange={(e) => setNames(e.target.value)}
-                />
-              </div>
-              <div className="form-group">
-                <label>Price ($)</label>
-                <input
-                  type="number"
-                  value={price}
-                  onChange={(e) => setPrice(e.target.value)}
-                />
-              </div>
-              <div className="form-group">
-                <label>Quantity</label>
-                <input
-                  type="number"
-                  value={quantity}
-                  onChange={(e) => setQuantity(e.target.value)}
-                />
-              </div>
-              <div className="price-display">
-                <p>
-                  The price is
-                  <span className="price-value">$ {price * quantity}</span>
-                </p>
-              </div>
-              <button>submit</button>
-            </form>
+        <button onClick={() => setPopup(true)}>+</button>
+        {popup && (
+          <div className="form-div">
+            <div className="div-border">
+              <button
+                className="close-popup-btn"
+                onClick={() => setPopup(false)}
+              >
+                &times;
+              </button>
+              <h2>Add New Item</h2>
+              <form onSubmit={addItem}>
+                <div className="form-group">
+                  <label>Item Name</label>
+                  <input
+                    type="text"
+                    value={names}
+                    placeholder="e.g Sugar"
+                    onChange={(e) => setNames(e.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Price ($)</label>
+                  <input
+                    type="number"
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Quantity</label>
+                  <input
+                    type="number"
+                    value={quantity}
+                    onChange={(e) => setQuantity(e.target.value)}
+                  />
+                </div>
+                <div className="price-display">
+                  <p>
+                    The price is
+                    <span className="price-value">$ {price * quantity}</span>
+                  </p>
+                </div>
+                <button type="submit">submit</button>
+              </form>
+            </div>
           </div>
+        )}
+        <div>
+          {items.map((item, key) => (
+            <div key={key}>{item.name}</div>
+          ))}
         </div>
       </div>
     </div>
